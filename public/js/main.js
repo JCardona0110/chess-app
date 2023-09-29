6d935d8221254c1067b411c69dd75cd98262cded
Jonathan Cardona <jcardona0110@gmail.com>
2023-09-28 20:28:20 -0400

    configured tailwindcss and fiveserver

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+const chessboard = document.querySelector('.chessboard')
+const turnDisplay = document.querySelector('.turnDisplay')
+const width = 8
+
+const startPieces = [
+  rook, knight, bishop, queen, king, bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  rook, knight, bishop, queen, king, bishop, knight, rook
+]
+const rook = 1 
+const knight = 2
+const bishop = 3
+const queen = 4
+const king = 5
+const pawn = 6
+
